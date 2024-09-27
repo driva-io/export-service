4f610459d1d7555ca5008f6117d153496d4c3578
Marcos Travasso <marcos.travasso42@gmail.com>
2024-09-27 11:05:05 -0300

    fix: get envs

--- getEnvs.py
+++ getEnvs.py
@@ -5,18 +5,19 @@ DOKKU_APP = "export-service-only"
 
 f = open(FILE, "r")
 
-lines = f.readlines()
+ls = f.readlines()
 
 envs = []
-for line in lines:
-    if line[0] == "#":
-        continue
-
-    key = line.split("=")[0]
-    value = line.split("=")[1]
-
-    enc = key + "=" + base64.b64encode(value.replace("\n", "").encode()).decode()
-    envs.append(enc)
+for l in ls:
+    if l[0] != "#" and "=" in l:
+        key = l.split("=")[0]
+        value = l.split("=")[1]
+        enc = (
+            key
+            + "="
+            + base64.b64encode(value.replace("\n", "").encode("ascii")).decode("ascii")
+        )
+        envs.append(enc)
 
 f.close()
 
